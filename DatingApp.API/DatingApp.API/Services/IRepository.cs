ee7601d6c29ede5959353b2814beebff66ea56c0
mzajac <matzaj101@gmail.com>
2018-12-02 11:10:35 +0100

    Initial Commit

---
+++ DatingApp.API/DatingApp.API/Services/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Services
+{
+    public interface IRepository
+    {
+        Task<IEnumerable<Value>> Values { get; }
+    }
+}
---
+++ DatingApp.API/DatingApp.API/Services/Repository.cs
@@ -0,0 +1,27 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Services
+{
+    public class Repository : IRepository
+    {
+        private readonly DataContext _context;
+
+        public Repository(DataContext context) => _context = context;
+
+        public IEnumerable<Value> Values => _context.Values.ToList();
+
+        Task<IEnumerable<Value>> IRepository.Values
+        {
+            get
+            {
+                return Task.FromResult<IEnumerable<Value>>(_context.Values);
+            }
+        }
+    }
+}
