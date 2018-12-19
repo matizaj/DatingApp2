7b570908b90edbdc675afef459c4cec2ec43e77d
mzajac <matzaj101@gmail.com>
2018-12-19 20:06:41 +0100

    quick fix

--- DatingApp-SPA/src/app/members/member-card/member-card.component.ts
+++ DatingApp-SPA/src/app/members/member-card/member-card.component.ts
@@ -3,7 +3,6 @@ import { User } from 'src/app/_models/user';
 import { AuthService } from 'src/app/_services/auth.service';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
-import { userInfo } from 'os';
 
 @Component({
   selector: 'app-member-card',
